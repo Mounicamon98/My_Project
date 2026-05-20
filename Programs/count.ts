efbc5f60c34910da73a0aeace2ae9a1e26c598e5
mounica <eerlamounica@gmail.com>
2026-05-20 11:40:37 +0530

    updated modified files

--- Programs/count.ts
+++ Programs/count.ts
@@ -1,9 +1,14 @@
-let x:number[]=[2,1,3,2,1,3]
-let c:any={};
-for(let i =0; i<x.length;i++)
+const arr1:number[]=[55,12,32,12]
+const obj:any={}
+for(let i=0;i<arr1.length;i++)
 {
-    if(c[x[i]]=0)
+    if(obj[arr1[i]])
     {
-
+        obj[arr1[i]]++;
+    }
+    else
+    {
+        obj[arr1[i]]=1
     }
 }
+console.log(obj);
