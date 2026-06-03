07fb0489ae7c948f4dd45cc1dc30773534b66de3
mounica <eerlamounica@gmail.com>
2026-06-03 15:33:56 +0530

    all changes programs reverse srtring

--- Programs/ReverseString.js
+++ Programs/ReverseString.js
@@ -1,3 +1,18 @@
 let name ="mounica";
+let arr = name.split("");//["m","o","u","n","i","c","a"]
+let left=0;
+let right=arr.length-1;
+while(left<right)
+{
+    let temp=arr[left];//temp = m
+    arr[left]=arr[right];//m=a
+    arr[right]=temp//a=m
+    left++;
+    right--;
+}
+console.log(arr.join(""));
+
+
+
 
 
